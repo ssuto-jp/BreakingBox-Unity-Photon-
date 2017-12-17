69390bb9a8b33f048ab508a03a1dd1cd09392f29
suto <ss.time2531@gmail.com>
2017-12-18 01:18:44 +0900

    プレイヤーの頭上に名前を表示

---
+++ Assets/BreakingBox/Scripts/PlayerNameUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameUI : Photon.MonoBehaviour
+{
+    private GameObject namePlate;
+    [SerializeField] private Text nameText;
+
+    private void Start()
+    {
+        namePlate = nameText.transform.parent.gameObject;
+        photonView.RPC("SetName", PhotonTargets.All, PhotonNetwork.playerName);
+    }
+
+    private void LateUpdate()
+    {
+        namePlate.transform.rotation = Camera.main.transform.rotation;
+    }
+
+    [PunRPC]
+    void SetName(string name)
+    {
+        nameText.text = name;
+    }
+
+}
