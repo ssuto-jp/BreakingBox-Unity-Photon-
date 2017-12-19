8e3b1fbb3d33841a753af3b6ec05c50975994108
suto <ss.time2531@gmail.com>
2017-12-19 13:53:30 +0900

    時間制限を追加

---
+++ Assets/BreakingBox/Scripts/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+
+    [SerializeField] private Text countDownText;
+    [SerializeField] private float totalTime = 20;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (PhotonNetwork.room.PlayerCount == 2)
+        {
+            CountDownTimer();
+        }
+
+    }
+
+    private void CountDownTimer()
+    {
+        totalTime -= Time.deltaTime;
+        if (totalTime <= 0)
+        {
+            totalTime = 0;
+        }
+        countDownText.text = totalTime.ToString("F2");
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(totalTime);
+        }
+        else
+        {
+            totalTime = (float)stream.ReceiveNext();
+        }
+    }
+}
